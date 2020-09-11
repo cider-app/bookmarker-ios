b034a914b693e3580215304ff518573fe9930855
kennethlng <ng.kennethlee@gmail.com>
2020-09-11 10:23:33 -0700

    Initial commit

---
+++ bookmarker-ios/bookmarker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookmarker_iosApp.swift
+//  bookmarker-ios
+//
+//  Created by Kenneth Ng on 9/11/20.
+//
+
+import SwiftUI
+
+@main
+struct bookmarker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
