f9493a801e175d434e84a92e8ca09e73872adfb8
kennethlng <ng.kennethlee@gmail.com>
2020-09-13 13:46:19 -0700

    Added currentUserIsMember flag, leave, and delete for FolderViewModel

--- bookmarker-ios/ViewModels/FolderViewModel.swift
+++ bookmarker-ios/ViewModels/FolderViewModel.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 import FirebaseFirestore
+import FirebaseAuth
 
 class FolderViewModel: ObservableObject {
     @Published var folder: Folder?
     @Published var folderFiles = [FolderFile]()
+    @Published var isLoading: Bool = false
+    @Published var currentUserIsMember: Bool = false
     var listener: ListenerRegistration?
     
     func listenFolder(folderId: String) {
@@ -42,5 +45,57 @@ class FolderViewModel: ObservableObject {
     
     func unlistenFolder() {
         listener?.remove()
+    }
+    
+    func leave(folderId: String, completion: ((Error?) -> Void)?) {
+        guard let authUser = Auth.auth().currentUser else { return }
+        
+        isLoading = true
+        
+        let batch = Firestore.firestore().batch()
+        
+        let userFolderRef = UserFolder.subcollectionRef(parentDocId: authUser.uid).document(folderId)
+        let folderUserRef = FolderUser.subcollectionRef(parentDocId: folderId).document(authUser.uid)
+        
+        batch.deleteDocument(userFolderRef)
+        batch.deleteDocument(folderUserRef)
+        
+        batch.commit { (error) in
+            self.isLoading = false
+            if let error = error {
+                print("Error removing document: \(error)")
+                completion?(error)
+            } else {
+                print("User folder successfully removed!")
+                completion?(nil)
+            }
+        }
+    }
+    
+    func delete(folderId: String, completion: ((Error?) -> Void)?) {
+        isLoading = true
+        Folder.collectionRef.document(folderId).delete { (error) in
+            self.isLoading = false
+            if let error = error {
+                print("Error removing document: \(error)")
+                completion?(error)
+            } else {
+                print("User folder successfully removed!")
+                completion?(nil)
+            }
+        }
+    }
+    
+    func getCurrentUserMembership(folderId: String) {
+        guard let authUser = Auth.auth().currentUser else { return }
+        
+        FolderUser.subcollectionRef(parentDocId: folderId).document(authUser.uid).getDocument { (documentSnapshot, error) in
+            if let document = documentSnapshot, document.exists {
+                self.currentUserIsMember = true
+            } else {
+                print("Document does not exist")
+                self.currentUserIsMember = false 
+            }
+        }
     }
 }
