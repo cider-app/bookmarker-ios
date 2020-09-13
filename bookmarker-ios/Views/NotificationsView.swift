68aff15840491bba8d5b0a0f12d96f06f340c867
kennethlng <ng.kennethlee@gmail.com>
2020-09-13 11:38:34 -0700

    Created basic NotificationsView for notifications tab

---
+++ bookmarker-ios/Views/NotificationsView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationsView.swift
+//  bookmarker-ios
+//
+//  Created by Kenneth Ng on 9/13/20.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Messages")
+        }
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
