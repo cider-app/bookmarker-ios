f2be43e6114d42101a6315ba66eeff87dea79695
kennethlng <ng.kennethlee@gmail.com>
2020-09-27 11:59:06 -0700

    Updated TextFieldViewModifier with variant options for filled and outlined

--- bookmarker-ios/ViewModifiers/TextFieldViewModifier.swift
+++ bookmarker-ios/ViewModifiers/TextFieldViewModifier.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 struct TextFieldViewModifier: ViewModifier {
+    enum Variant {
+        case outlined, filled
+    }
+    
+    var variant: Variant = .outlined
+    
     func body(content: Content) -> some View {
         content
             .font(Font.system(.body).weight(Constants.fontWeight))
             .padding()
-            .overlay(RoundedRectangle(cornerRadius: Constants.cornerRadius, style: .continuous).stroke(Color(.systemGray6), lineWidth: 2))
+            .overlay(variant == Variant.outlined ?
+                RoundedRectangle(cornerRadius: Constants.cornerRadius, style: .continuous).stroke(Color(.systemGray6), lineWidth: 2)
+                : nil
+            )
+            .background(variant == Variant.filled ?
+                RoundedRectangle(cornerRadius: Constants.cornerRadius, style: .continuous).fill(Color(.systemGray6))
+                : nil
+            )
     }
 }
