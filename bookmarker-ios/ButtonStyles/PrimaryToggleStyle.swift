3d1da8e06e327c6964d6d6e75255474186d2e294
kennethlng <ng.kennethlee@gmail.com>
2020-09-30 01:20:54 -0700

    Update PrimaryToggleStyle.swift

--- bookmarker-ios/ButtonStyles/PrimaryToggleStyle.swift
+++ bookmarker-ios/ButtonStyles/PrimaryToggleStyle.swift
@@ -11,17 +11,18 @@ struct PrimaryToggleStyle: ToggleStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
             configuration.label
+                .font(Font.system(.headline).weight(.semibold))
             Spacer()
             Button(action: {
                 configuration.isOn.toggle()
             }) {
-                RoundedRectangle(cornerRadius: Constants.cornerRadius)
-                    .fill(configuration.isOn ? Color.primaryColor : Color(.systemGray5))
-                    .frame(width: 50, height: 29)
+                RoundedRectangle(cornerRadius: Constants.cornerRadius, style: .continuous)
+                    .fill(configuration.isOn ? Color.primaryColor : Color(.systemGray6))
+                    .frame(width: 50, height: 32)
                     .overlay(
-                        RoundedRectangle(cornerRadius: Constants.cornerRadius)
+                        RoundedRectangle(cornerRadius: Constants.cornerRadius, style: .continuous)
                             .fill(Color.white)
-                            .frame(width: 25, height: 25)
+                            .frame(width: 26, height: 26)
                             .padding(2)
                             .offset(x: configuration.isOn ? 10 : -10, y: 0)
                     )
