6c932727768739c74356706859866c1e9131500a
kennethlng <ng.kennethlee@gmail.com>
2020-09-30 11:28:41 -0700

    Removed unused colors

--- bookmarker-ios/Color.swift
+++ bookmarker-ios/Color.swift
@@ -9,9 +9,5 @@ import Foundation
 import SwiftUI
 
 extension Color {
-    static let ColorA71D31 = Color("A71D31")
-    static let Color4B8F8C = Color("4B8F8C")
-    static let Color9A7AA0 = Color("9A7AA0")
-    static let Color020887 = Color("020887")
     static let primaryColor = Color("PrimaryColor")
 }
